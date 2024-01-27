df993cc38294ae79dfc77627c61f3d2548465e30
Hysocs <143819956+Hysocs@users.noreply.github.com>
2024-01-27 05:39:15 -0500

    Update Config.py (Folder Gen and syntax fixes)
    When the program is executed for the first time, it will now check for the existence of the 'CrossPixel' temporary folder. If the folder is not present, the program will automatically generate it. This new approach has been implemented to address issues encountered in the previous system. A standalone function has been created to handle this task, which helps prevent potential issues in the future.

--- Components/Settings/Config.py
+++ Components/Settings/Config.py
@@ -18,23 +18,28 @@ appdata_local_path = os.path.join(os.path.expanduser("~"), "AppData", "Local")
 CROSSPIXEL_DIR_PATH = os.path.join(appdata_local_path, "CrossPixel")
 KEYBINDS_FILE_PATH = os.path.join(CROSSPIXEL_DIR_PATH, "keybinds_config.json")
 
-def save_keybinds_to_file(keybinds):
-    """Save keybinds to a file in the CrossPixel directory within the AppData\Local directory."""
-    # Ensure the CrossPixel directory exists
+def ensure_crosspixel_folder_exists():
+    r"""Ensure the CrossPixel directory exists within the AppData\Local directory."""
     if not os.path.exists(CROSSPIXEL_DIR_PATH):
         try:
             os.makedirs(CROSSPIXEL_DIR_PATH)
-        except OSError:
-            # If directory creation fails, return the default keybinds
-            return DEFAULT_KEYBINDS
+            print("CrossPixel directory created successfully.")
+        except OSError as e:
+            print(f"Error creating CrossPixel directory: {e}")
+
+def save_keybinds_to_file(keybinds):
+    r"""Save keybinds to a file in the CrossPixel directory within the AppData\Local directory."""
+    ensure_crosspixel_folder_exists()
     
     # Save the keybinds to the file
     with open(KEYBINDS_FILE_PATH, 'w') as file:
         json.dump(keybinds, file)
 
 def load_keybinds_from_file():
-    """Load keybinds from the file in the CrossPixel directory within the AppData\Local directory. 
+    r"""Load keybinds from the file in the CrossPixel directory within the AppData\Local directory. 
        Return default keybinds if file doesn't exist or there's an error reading it."""
+    ensure_crosspixel_folder_exists()
+
     # If the file doesn't exist, return default keybinds
     if not os.path.exists(KEYBINDS_FILE_PATH):
         return DEFAULT_KEYBINDS
@@ -43,6 +48,6 @@ def load_keybinds_from_file():
     try:
         with open(KEYBINDS_FILE_PATH, 'r') as file:
             return json.load(file)
-    except:
-        # If there's an error reading the file, return the default keybinds
+    except Exception as e:
+        print(f"Error reading file: {e}")
         return DEFAULT_KEYBINDS
